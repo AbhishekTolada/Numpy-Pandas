4265beca144b729c2ca39c359f22d650ac4b21ce
AbhishekTolada <abhishektolada3589@gmail.com>
2026-03-03 10:09:24 +0530

    Adding files

---
+++ Pandas/pandas2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+        "Name" : ["Abhi", "Vivi", "John"],
+        "Age" : ["10", "20", "30"],
+        "City" : ["vizag", "hyderabad", "USA"]
+}
+
+new_data = pd.DataFrame(data)
+
+print(new_data)
